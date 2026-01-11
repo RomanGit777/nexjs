78c4818ce2aaa94e067c75dc4220e7f0b67f60f9
RomanGit777 <rsavenko91@gmail.com>
2026-01-11 13:12:00 +0100

    hw/hw1/ menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Menu from "@/menu/page";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
       {children}
+      <Menu/>
       </body>
     </html>
   );
---
+++ src/menu/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Menu = () => {
+    return (
+        <ul>
+            <li><Link href={'/'}>home</Link></li>
+            <li><Link href={'/users'}>users</Link></li>
+            <li><Link href={'/posts'}>posts</Link></li>
+            <li><Link href={'/users/id'}>usersId</Link></li>
+            <li><Link href={'/posts/id'}>postsId</Link></li>
+            <li><Link href={'/comments/id'}>comments/Id</Link></li>
+        </ul>
+    );
+};
+export default Menu;
