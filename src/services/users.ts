d1c7759a14b0b29edb3b4459d9acf8250ac79512
RomanGit777 <rsavenko91@gmail.com>
2026-01-11 14:34:00 +0100

    hw/hw1/ users service

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,7 +1,14 @@
 import {IUser} from "@/models/IUser";
 import {baseUrl} from "@/services/base";
 
-export const getUsers = async (): Promise<IUser[]> => {
-    const users= await fetch(baseUrl+'/users').then(res => res.json())
-    return users;
+export async function getAllUsers(): Promise<IUser[]> {
+    const res = await fetch(`${baseUrl}/users`);
+    if (!res.ok) throw new Error(`Failed to fetch users`);
+    return res.json();
+}
+
+export async function getUserById(id: string): Promise<IUser> {
+    const res = await fetch(`${baseUrl}/users/${id}`);
+    if (!res.ok) throw new Error('Failed to fetch users');
+    return res.json();
 }
